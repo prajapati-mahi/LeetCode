4fa742af07c737c0ab11cb00ca7ad1e693a42a3b
prajapati-mahi <mahi.23bce10098@vitbhopal.ac.in>
2026-08-07 22:43:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.36%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        while(n>0){
+            int last= n%2;
+            if(last==1){
+                count++;
+            }
+            n=n/2;
+        }
+        
+        return count;
+    }
+};
