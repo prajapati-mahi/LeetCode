9467632eff0f48462a4c2f117d0e6f9d89e09f9f
Mahi Prajapati <mahi.23bce10098@vitbhopal.ac.in>
2026-08-26 00:10:28 +0530

    Time: 0 ms (100%), Space: 11.5 MB (32.93%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int n= heights.size();
+        vector<int> sorted= heights;
+
+        sort(sorted.begin(), sorted.end());
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(sorted[i] != heights[i]){
+                count++;
+            }
+        }
+        return count;        
+    }
+};
