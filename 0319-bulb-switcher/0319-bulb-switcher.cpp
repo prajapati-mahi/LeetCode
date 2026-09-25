44be9f8a1641f1a4d374387b7f1bb856a6ce3c4e
Mahi Prajapati <mahi.23bce10098@vitbhopal.ac.in>
2026-09-25 11:24:01 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.35%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // if(n==0){
+        //     return 0;
+        // }
+        // if(n==1){
+        //     return 1;
+        // }
+        // int ans=0;
+        // for(int i=1; i<=n; i++){
+        //     int count=0;
+        //     for(int j=1; j<=i; j++){
+        //         if(i%j ==0){
+        //             count++;
+        //         }
+        //     }
+        //     if(count%2 == 1){
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+        return sqrt(n);
+        // int count=0;
+        // for(int i=0; i<n; i++){
+        //     if(arr[i]==1){
+        //         count++;
+        //     }
+        // }
+        // return count;
+    }
+};
